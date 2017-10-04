5d2267b998ac6e7f8e060dd4ad4c63e98e1a363a
Attie, Raphael (GSFC-6710)[UNIVERSITIES SPACE RESEARCH ASSOCIATION] <raphael.attie@nasa.gov>
2017-10-04 11:30:44 -0400

    - query_spikes uses a more advanced and efficient python method for download.

--- DRMS/query_spikes.py
+++ DRMS/query_spikes.py
@@ -1,7 +1,9 @@
 import drms
+import os
+import urllib
 
-# Local directory for download
-download_dir = '~/Data'
+# Local directory for download. E.g: ~/Data
+download_dir = os.environ['HOME'] + '/Data'
 # Instantiate a drms client
 c = drms.Client(email='raphael.attie@nasa.gov', verbose=True)
 # Request an export. Example with just 10 minutes of spikes for 171
@@ -9,11 +11,18 @@ r_171 = c.export('aia.lev1_euv_12s[2010.05.13_00:00/1h][171]{spikes}', method='u
 r_171.wait()
 
 if r_171.status != 0:
-    ## If needed, get the url of the tar file (for download at a later time with other download libraries like "urllib")
-    # url_171 = r_171.urls['url'][0]
-
-    ## Download using the drms module
-    r_171.download(download_dir)
-
-else:
+    # If this is in a function, consider doing a sys.exit() to issue an error message and exit.
     print("Problem with the export request")
+else:
+    ##  Download using the drms module. See below for alternate download method
+    # r_171.download(download_dir)
+
+    ## If needed, get the url of the tar file (for download at a later time with other download libraries like "urllib")
+    url_171, filename = r_171.urls['url'][0], r_171.urls['filename'][0]
+    # Build a local file path using the exported tar filename
+    local_filepath = os.path.join(download_dir, filename)
+    # Download using urllib.
+    urllib.request.urlretrieve(url_171, local_filepath)
+    # Clear the cache that may have built up
+    urllib.request.urlcleanup()
+
