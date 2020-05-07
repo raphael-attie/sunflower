73d6c0ed37b9e4afb967e9e9820ab5640f71d2b7
Raphael Attie <attie.raphael@gmail.com>
2020-05-07 16:45:57 -0400

    updated velocity calibration for ARGO for any number of files

--- optimization/parameter_sweep_velocity_calibration.py
+++ optimization/parameter_sweep_velocity_calibration.py
@@ -51,16 +51,13 @@ def calc_c_pearson(vx1, vx2, vy1, vy2, fov=None):
 # output directory for the drifting images
 datadir = os.path.join(os.environ['DATA'], 'sanity_check/stein_series/calibration')
 datadir_stein = os.path.join(os.environ['DATA'], 'Ben/SteinSDO/')
-
 # number of parameter sets
-nsets = 3600
-filelist = [os.path.join(datadir, 'param_sweep_{:d}.csv'.format(idx)) for idx in range(nsets)]
+
+filelist = glob.glob(os.path.join(datadir, 'param_sweep_*.csv'))
 # Concatenate all csv file content into one dataframe
 df_list = [pd.read_csv(f) for f in filelist]
 df = pd.concat(df_list, axis=0, ignore_index=True)
 df.set_index('index', inplace=True)
-# List of velocity flow files
-filelist = [os.path.join(datadir, 'mean_velocity_{:d}.npz'.format(idx)) for idx in range(nsets)]
 
 df['a_top_0'] = 1 / df['p_top_0']
 df['a_bot_0'] = 1 / df['p_bot_0']
@@ -77,6 +74,10 @@ svy_files = sorted(glob.glob(os.path.join(datadir_stein,'SDO_vy*.fits')))
 vx_stein, vy_stein = load_vel_mean((svx_files, svy_files), trange)
 # smooth the Stein velocities
 vx_stein_sm, vy_stein_sm = smooth_vel(vx_stein, vy_stein, fwhm, kernel='boxcar')
+
+
+# List of balltracked velocity flows
+filelist = glob.glob(os.path.join(datadir, 'mean_velocity*.npz'))
 
 corr_uncalibrated = []
 correlations = []
