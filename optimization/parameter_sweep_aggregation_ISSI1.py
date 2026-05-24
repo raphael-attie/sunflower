80a6afa8aa5df48dbbd888959cda8fe1cf1020a3
Raphael Attie <attie.raphael@gmail.com>
2026-05-23 23:03:02 -0400

    updated velocity axis name for the aggregation script

--- optimization/parameter_sweep_aggregation_ISSI1.py
+++ optimization/parameter_sweep_aggregation_ISSI1.py
@@ -88,8 +88,8 @@ fwhm = inputs.maps_params['fwhm']
 trim = 10 # Same as Benoit
 fov = np.s_[trim:-trim:fwhm, trim:-trim:fwhm]
 # Get the ground truth velocity (simulation)
-svx_files = sorted(Path(inputs.inputdir).glob('SDO_vx*.fits'))
-svy_files = sorted(Path(inputs.inputdir).glob('SDO_vy*.fits'))
+svx_files = sorted(Path(inputs.inputdir).glob('vy_out*.fits'))
+svy_files = sorted(Path(inputs.inputdir).glob('vz_out*.fits'))
 vx_sim, vy_sim = load_vel_mean((svx_files, svy_files), trange)
 # smooth the simulation velocities
 kernel = inputs.maps_params['kernel']
